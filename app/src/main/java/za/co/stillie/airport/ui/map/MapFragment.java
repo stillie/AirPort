c2724a9be6ee3cfd24cb31b2b50cc002a8a48f02
Ryan van der Westhuizen <ryan.vanderwesthuizen@cartrack.com>
2019-05-03 14:49:45 +0200

    Initial Commit

--- app/src/main/java/za/co/stillie/airport/ui/map/MapFragment.java
+++ app/src/main/java/za/co/stillie/airport/ui/map/MapFragment.java
@@ -5,20 +5,26 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
 import za.co.stillie.airport.R;
 import za.co.stillie.airport.base.BaseFragment;
 
-public class MapFragment extends BaseFragment {
+public class MapFragment extends BaseFragment implements OnMapReadyCallback {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MapViewModel mapViewModel = ViewModelProviders.of(this).get(MapViewModel.class);
-        mapViewModel.onCreate(getFragmentManager());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap aGoogleMap) {
 
     }
 }
--- app/src/main/java/za/co/stillie/airport/ui/map/MapViewModel.java
+++ app/src/main/java/za/co/stillie/airport/ui/map/MapViewModel.java
@@ -4,20 +4,20 @@ import android.support.v4.app.FragmentManager;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 
-import za.co.stillie.airport.R;
 import za.co.stillie.airport.base.BaseViewModel;
 
 public class MapViewModel extends BaseViewModel implements OnMapReadyCallback {
 
+
     void onCreate(FragmentManager aFragmentManager) {
-        SupportMapFragment mapFragment = (SupportMapFragment) aFragmentManager.findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap aGoogleMap) {
+    }
 
+    private void moveToLocation(GoogleMap aGoogleMap) {
+        // move camera to location
     }
 }
